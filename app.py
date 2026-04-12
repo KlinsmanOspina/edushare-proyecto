5005421e1f2b7eb589b3631efb840bf994bbfd5c
KlinsmanOspina <klinsmanospina@gmail.com>
2026-04-12 17:14:24 -0500

    Update Supabase connection information
    Updated Supabase connection details with actual URL and key.

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from supabase import create_client, Client
 st.set_page_config(page_title="EduShare | Proyecto de Grado", page_icon="🎓", layout="wide")
 
 # 2. CONEXIÓN A SUPABASE (RELLENA TUS DATOS AQUÍ)
-URL_SUPABASE = "PON_AQUI_TU_URL"
-KEY_SUPABASE = "PON_AQUI_TU_LLAVE_ANON"
+URL_SUPABASE = "https://cofdpzmpkwiybeffqntk.supabase.co"
+KEY_SUPABASE = "sb_publishable_zzPPoqndQZyUpwrnJ45ifg_vT0sfy6S"
 
 @st.cache_resource
 def init_connection():
